57e2dbeec5409148109c2271c151a80d554e29e6
wahyu.g <wahyu.g@950e56f8-7d76-0c82-4ab9-745da9058755>
2012-04-20 19:56:14 +0000

    Fixed click trigger on South Carolina. Fixed logReader

--- scripts/logReader.php
+++ scripts/logReader.php
@@ -4,13 +4,14 @@ $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 if (!$db) die ('Database offline. Please check back later...');
 
 $sql = 'SELECT * FROM Log';
-echo '<pre>';
+echo '<html><body><pre>';
 foreach ($db->query($sql) as $row) {
 	$humanDate = getdate($row['Timestamp']);
 	echo $humanDate['year'] .'-' .$humanDate['month'] .'-' .$humanDate['mday']
+		.' ' .$humanDate['hours'] .':' .$humanDate['minutes']
 		.': ' .$row['IPAddress'] .' (' 
-		.$row['Seeking'] .'|' .$row['States'] .'|' .$row['Nudity'] .'|' .$row['Compensation']
-		.') ' .$row['UserAgent'] .'\n'; 
+		.$row['Seeking'] .'|' .$row['Country'] .'|' .$row['States'] .'|' .$row['Nudity'] .'|' .$row['Compensation']
+		.') ' .$row['UserAgent'] .'<br />'; 
 }
-echo '</pre>';
+echo '</pre></body></html>';
 ?>
--- scripts/logger.php
+++ scripts/logger.php
@@ -1,10 +1,12 @@
 <?php
 function logger($db, $t) {
-	$query = 'INSERT INTO Log VALUES('
+	$query = 'INSERT INTO Log ("Timestamp", "UserAgent", "IPAddress", "Seeking", "Country", "States", "Nudity", "Compensation")
+			VALUES('
 			.$t['Timestamp'] .', '
 			.'"' .$t['UserAgent'] .'", '
 			.'"' .$t['IPAddress'] .'", '
 			.$t['Seeking'] .', '
+			.'"' .$t['Country'] .'", '
 			.'"' .$t['States'] .'", '
 			.$t['Nudity'] .', '
 			.'"' .$t['Compensation'] .'")';
